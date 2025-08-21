5d2e2f79340e6b78a56abd2382ea2c5fdd4e36a7
Abdullah Ahmad Arslan <abdullah.ahmad.arslan@gmail.com>
2025-08-21 11:07:44 +0500

    router file conflict resolved

--- apps/api/app/api/v1/router.py
+++ apps/api/app/api/v1/router.py
@@ -2,16 +2,9 @@ from fastapi import APIRouter
 from app.api.v1 import auth
 from app.api.v1 import shopping
 api_router = APIRouter()
-from app.api.v1 import meal_planning
+
 # Auth routes
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 
 #Shopping List routes
 api_router.include_router(shopping.router, prefix="/shopping_list", tags=["shopping_list"])
-
-# Meal planning routes
-api_router.include_router(
-    meal_planning.router, 
-    prefix="/meal-planning", 
-    tags=["meal-planning"]
-)
